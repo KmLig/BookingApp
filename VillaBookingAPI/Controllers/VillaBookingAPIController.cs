cab5c5500bd94c6a00a3d1eeaa38f506e8bd7932
Khiem <phamxuankhiem1102@gmail.com>
2025-02-22 19:38:15 +0700

    Create the first endpoint for get all villas

--- VillaBookingAPI/Controllers/VillaBookingAPIController.cs
+++ VillaBookingAPI/Controllers/VillaBookingAPIController.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VillaBookingAPI.Models;
 
 namespace VillaBookingAPI.Controllers
 {
+    [Route("api/VillaBookingAPI")]
     [ApiController]
     public class VillaBookingAPIController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa{Id = 1, Name="Pool view"},
+                new Villa{Id = 2, Name = "River view"}
+            };
+        }
     }
 }
