76ca76ffb3845f834895e75da842fda062209edb
Khiem <phamxuankhiem1102@gmail.com>
2025-02-23 21:17:10 +0700

    Document the response type for GetVilla

--- VillaBookingAPI/Controllers/VillaBookingAPIController.cs
+++ VillaBookingAPI/Controllers/VillaBookingAPIController.cs
@@ -11,6 +11,9 @@ namespace VillaBookingAPI.Controllers
     public class VillaBookingAPIController : ControllerBase
     {
         [HttpGet("{id:int}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
         public ActionResult<VillaDTO> GetVilla(int id)
         {
             if (id == 0)
