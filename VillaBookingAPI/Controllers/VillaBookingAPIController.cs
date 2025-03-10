2e00a0ae10bc94ed1690648315b972dd5cc9c91c
Khiem <phamxuankhiem1102@gmail.com>
2025-03-10 20:05:55 +0700

    Create DeleteVilla endpoint to delete a villa by id

--- VillaBookingAPI/Controllers/VillaBookingAPIController.cs
+++ VillaBookingAPI/Controllers/VillaBookingAPIController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VillaBookingAPI.Data;
-using VillaBookingAPI.Models;
 using VillaBookingAPI.Models.Dto;
 
 namespace VillaBookingAPI.Controllers
@@ -16,54 +15,75 @@ namespace VillaBookingAPI.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
         {
-
             // if we don't use the apicontroller attr we have to validate the model by ourselves using ModelState
             //if (!ModelState.IsValid)
             //{
             //    return BadRequest(ModelState);
             //}
+            if(villaDTO == null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
 
-            if(VillaStore.villaList.FirstOrDefault(v => v.Name.Equals(villaDTO.Name, StringComparison.CurrentCultureIgnoreCase)) != null)
+            if(VillaStore.villaList
+                    .FirstOrDefault(v => v.Name.Equals(villaDTO.Name, StringComparison.CurrentCultureIgnoreCase)) !=
+                null)
             {
                 ModelState.AddModelError("CustomError", "Villa already exists");
                 return BadRequest(ModelState);
             }
 
-            if (villaDTO == null)
-            {
-                return StatusCode(StatusCodes.Status400BadRequest);
-            }
-            if (villaDTO.Id > 0)
+            if(villaDTO.Id > 0)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
-            villaDTO.Id = VillaStore.villaList.OrderByDescending(v => v.Id).FirstOrDefault().Id + 1;
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(v => v.Id).FirstOrDefault().Id  + 1;
             VillaStore.villaList.Add(villaDTO);
 
             return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
         }
+
+        [HttpDelete("{id:int}", Name = "DeleteVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteVilla(int id)
+        {
+            // with ActionResult we can return a type
+            // with IActionResult we can return a status code with no content
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+            if(villa == null)
+            {
+                return NotFound();
+            }
+            VillaStore.villaList.Remove(villa);
+            return NoContent();
+        }
+
         [HttpGet("{id:int}", Name = "GetVilla")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<VillaDTO> GetVilla(int id)
         {
-            if (id == 0)
+            if(id == 0)
             {
                 return BadRequest();
             }
             var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
-            if (villa == null)
+            if(villa == null)
             {
                 return NotFound();
             }
             return Ok(villa);
         }
+
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
-        {
-            return Ok(VillaStore.villaList);
-        }
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas() { return Ok(VillaStore.villaList); }
     }
 }
